a1778eac0e7b9c01f3d63c67ae5438675c542362
Egor <egorka.leps@mail.ru>
2022-03-27 19:34:01 +0700

    errors v2

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::group(
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('user.refresh');
 }
 );
-// test 223
+// test hferr
 Route::group(
     [
       //  'middleware' => 'admin',
